7dc17ae0f37871241282cc1c57c360229dbdcf57
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-12 15:36:15 +0000

    Support implementation...

--- main.py
+++ main.py
@@ -70,5 +70,5 @@ while is_on:
         if is_resource_sufficient(drink['ingredients']):
             payment = process_coins()
             print(f"Your {choice} cost {drink['cost']}")
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffee(choice, drink["ingredients"])
+            if is_transaction_successful(payment, drink['cost']):
+                make_coffee(choice, drink['ingredients'])
